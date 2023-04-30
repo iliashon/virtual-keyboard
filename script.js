0115e7c5ad361bbd6a27872af9e45b5815e5dc7c
Tupalski Ilya <itupalski@gmail.com>
2023-04-30 12:25:43 +0400

    feat: add listener shift (Sun Apr 30 12:25:43 +04 2023)

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// let shiftNum = 2;
-// let altNum = 2;
-
-// const checkBtn = ['Backspace','Tab','Enter','CapsLock','Shift','Control','Alt','Meta', ' ']
-// const clickCodeEn = ['§', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Enter', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Shift', '`', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift', 'Control', 'Alt', 'Meta', ' ', 'Meta', 'Alt', '←', '↓', '↑', '→']
-// const clickCodeRu = ['±', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Enter', 'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', "э", 'Shift', 'ё', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', 'Shift', 'Control', 'Alt', 'Meta', ' ', 'Meta', 'Alt', '←', '↓', '↑', '→']
-
 window.onkeydown = evt => {
     if (evt.key == 'Tab') {
         evt.preventDefault();
@@ -14,7 +7,9 @@ window.onkeydown = evt => {
 }
 
 const buttonKeyEn = [['Backquote', '§'], ['Digit1', '1'], ['Digit2', '2'], ['Digit3', '3'], ['Digit4', '4'], ['Digit5', '5'], ['Digit6', '6'], ['Digit7', '7'], ['Digit8', '8'], ['Digit9', '9'], ['Digit0', '0'], ['Minus', '-'], ['Equal', '='], ['Backspace', 'Backspace'], ['Tab', 'Tab'], ['KeyQ', 'q'], ['KeyW', 'w'], ['KeyE', 'e'], ['KeyR', 'r'], ['KeyT', 't'], ['KeyY', 'y'], ['KeyU', 'u'], ['KeyI', 'i'], ['KeyO', 'o'], ['KeyP', 'p'], ['BracketLeft', '['], ['BracketRight', ']'], ['Backslash', '\\'], ['Enter', 'Enter'], ['CapsLock', 'CapsLock'], ['KeyA', 'a'], ['KeyS', 's'], ['KeyD', 'd'], ['KeyF', 'f'], ['KeyG', 'g'], ['KeyH', 'h'], ['KeyJ', 'j'], ['KeyK', 'k'], ['KeyL', 'l'], ['Semicolon', ';'], ['Quote', "'"], ['ShiftLeft', 'Shift'], ['IntlBackslash', '`'], ['KeyZ', 'z'], ['KeyX', 'x'], ['KeyC', 'c'], ['KeyV', 'v'], ['KeyB', 'b'], ['KeyN', 'n'], ['KeyM', 'm'], ['Comma', ','], ['Period', '.'], ['Slash', '/'], ['ShiftRight', 'Shift'], ['ControlLeft', 'Ctrl'], ['AltLeft', 'Alt'], ['MetaLeft', 'Cmd'], ['Space', ' '], ['MetaRight', 'Cmd'], ['AltRight', 'Alt'], ['ArrowLeft', '←'], ['ArrowUp', '↑'], ['ArrowDown', '↓'], ['ArrowRight', '→']];
-const buttonKeyRu = [['Backquote', '±'], ['Digit1', '1'], ['Digit2', '2'], ['Digit3', '3'], ['Digit4', '4'], ['Digit5', '5'], ['Digit6', '6'], ['Digit7', '7'], ['Digit8', '8'], ['Digit9', '9'], ['Digit0', '0'], ['Minus', '-'], ['Equal', '='], ['Backspace', 'Backspace'], ['Tab', 'Tab'], ['KeyQ', 'й'], ['KeyW', 'ц'], ['KeyE', 'у'], ['KeyR', 'к'], ['KeyT', 'е'], ['KeyY', 'н'], ['KeyU', 'г'], ['KeyI', 'ш'], ['KeyO', 'щ'], ['KeyP', 'з'], ['BracketLeft', 'х'], ['BracketRight', 'ъ'], ['Backslash', '\\'], ['Enter', 'Enter'], ['CapsLock', 'CapsLock'], ['KeyA', 'ф'], ['KeyS', 'ы'], ['KeyD', 'в'], ['KeyF', 'а'], ['KeyG', 'п'], ['KeyH', 'р'], ['KeyJ', 'о'], ['KeyK', 'л'], ['KeyL', 'д'], ['Semicolon', 'ж'], ['Quote', "э"], ['ShiftLeft', 'Shift'], ['IntlBackslash', '`'], ['KeyZ', 'я'], ['KeyX', 'ч'], ['KeyC', 'с'], ['KeyV', 'м'], ['KeyB', 'и'], ['KeyN', 'т'], ['KeyM', 'ь'], ['Comma', 'б'], ['Period', 'ю'], ['Slash', '/'], ['ShiftRight', 'Shift'], ['ControlLeft', 'Ctrl'], ['AltLeft', 'Alt'], ['MetaLeft', 'Cmd'], ['Space', ' '], ['MetaRight', 'Cmd'], ['AltRight', 'Alt'], ['ArrowLeft', '←'], ['ArrowUp', '↑'], ['ArrowDown', '↓'], ['ArrowRight', '→']];
+const buttonKeyEnShift = [['Backquote', '<'], ['Digit1', '!'], ['Digit2', '@'], ['Digit3', '#'], ['Digit4', '$'], ['Digit5', '%'], ['Digit6', '^'], ['Digit7', '&'], ['Digit8', '*'], ['Digit9', '('], ['Digit0', ')'], ['Minus', '_'], ['Equal', '+'], ['Backspace', 'Backspace'], ['Tab', 'Tab'], ['KeyQ', 'Q'], ['KeyW', 'W'], ['KeyE', 'E'], ['KeyR', 'R'], ['KeyT', 'T'], ['KeyY', 'Y'], ['KeyU', 'U'], ['KeyI', 'I'], ['KeyO', 'O'], ['KeyP', 'P'], ['BracketLeft', '{'], ['BracketRight', '}'], ['Backslash', '|'], ['Enter', 'Enter'], ['CapsLock', 'CapsLock'], ['KeyA', 'A'], ['KeyS', 'S'], ['KeyD', 'D'], ['KeyF', 'F'], ['KeyG', 'G'], ['KeyH', 'H'], ['KeyJ', 'J'], ['KeyK', 'K'], ['KeyL', 'L'], ['Semicolon', ':'], ['Quote', '"'], ['ShiftLeft', 'Shift'], ['IntlBackslash', '~'], ['KeyZ', 'Z'], ['KeyX', 'X'], ['KeyC', 'C'], ['KeyV', 'V'], ['KeyB', 'B'], ['KeyN', 'N'], ['KeyM', 'M'], ['Comma', '<'], ['Period', '>'], ['Slash', '?'], ['ShiftRight', 'Shift'], ['ControlLeft', 'Ctrl'], ['AltLeft', 'Alt'], ['MetaLeft', 'Cmd'], ['Space', ' '], ['MetaRight', 'Cmd'], ['AltRight', 'Alt'], ['ArrowLeft', '←'], ['ArrowUp', '↑'], ['ArrowDown', '↓'], ['ArrowRight', '→']];
+const buttonKeyRu = [['Backquote', '±'], ['Digit1', '1'], ['Digit2', '2'], ['Digit3', '3'], ['Digit4', '4'], ['Digit5', '5'], ['Digit6', '6'], ['Digit7', '7'], ['Digit8', '8'], ['Digit9', '9'], ['Digit0', '0'], ['Minus', '-'], ['Equal', '='], ['Backspace', 'Backspace'], ['Tab', 'Tab'], ['KeyQ', 'й'], ['KeyW', 'ц'], ['KeyE', 'у'], ['KeyR', 'к'], ['KeyT', 'е'], ['KeyY', 'н'], ['KeyU', 'г'], ['KeyI', 'ш'], ['KeyO', 'щ'], ['KeyP', 'з'], ['BracketLeft', 'х'], ['BracketRight', 'ъ'], ['Backslash', 'ё'], ['Enter', 'Enter'], ['CapsLock', 'CapsLock'], ['KeyA', 'ф'], ['KeyS', 'ы'], ['KeyD', 'в'], ['KeyF', 'а'], ['KeyG', 'п'], ['KeyH', 'р'], ['KeyJ', 'о'], ['KeyK', 'л'], ['KeyL', 'д'], ['Semicolon', 'ж'], ['Quote', "э"], ['ShiftLeft', 'Shift'], ['IntlBackslash', '`'], ['KeyZ', 'я'], ['KeyX', 'ч'], ['KeyC', 'с'], ['KeyV', 'м'], ['KeyB', 'и'], ['KeyN', 'т'], ['KeyM', 'ь'], ['Comma', 'б'], ['Period', 'ю'], ['Slash', '/'], ['ShiftRight', 'Shift'], ['ControlLeft', 'Ctrl'], ['AltLeft', 'Alt'], ['MetaLeft', 'Cmd'], ['Space', ' '], ['MetaRight', 'Cmd'], ['AltRight', 'Alt'], ['ArrowLeft', '←'], ['ArrowUp', '↑'], ['ArrowDown', '↓'], ['ArrowRight', '→']];
+const buttonKeyRuShift = [['Backquote', '>'], ['Digit1', '!'], ['Digit2', '"'], ['Digit3', '№'], ['Digit4', '%'], ['Digit5', ':'], ['Digit6', ','], ['Digit7', '.'], ['Digit8', ';'], ['Digit9', '('], ['Digit0', ')'], ['Minus', '_'], ['Equal', '+'], ['Backspace', 'Backspace'], ['Tab', 'Tab'], ['KeyQ', 'Й'], ['KeyW', 'Ц'], ['KeyE', 'У'], ['KeyR', 'К'], ['KeyT', 'Е'], ['KeyY', 'Н'], ['KeyU', 'Г'], ['KeyI', 'Ш'], ['KeyO', 'Щ'], ['KeyP', 'З'], ['BracketLeft', 'Х'], ['BracketRight', 'Ъ'], ['Backslash', 'Ё'], ['Enter', 'Enter'], ['CapsLock', 'CapsLock'], ['KeyA', 'Ф'], ['KeyS', 'Ы'], ['KeyD', 'В'], ['KeyF', 'А'], ['KeyG', 'П'], ['KeyH', 'Р'], ['KeyJ', 'О'], ['KeyK', 'Л'], ['KeyL', 'Д'], ['Semicolon', 'Ж'], ['Quote', "Э"], ['ShiftLeft', 'Shift'], ['IntlBackslash', '['], ['KeyZ', 'Я'], ['KeyX', 'Ч'], ['KeyC', 'С'], ['KeyV', 'М'], ['KeyB', 'И'], ['KeyN', 'Т'], ['KeyM', 'Ь'], ['Comma', 'Б'], ['Period', 'Ю'], ['Slash', '?'], ['ShiftRight', 'Shift'], ['ControlLeft', 'Ctrl'], ['AltLeft', 'Alt'], ['MetaLeft', 'Cmd'], ['Space', ' '], ['MetaRight', 'Cmd'], ['AltRight', 'Alt'], ['ArrowLeft', '←'], ['ArrowUp', '↑'], ['ArrowDown', '↓'], ['ArrowRight', '→']];
 const checkBtn = ['Backspace', 'Tab', 'Enter', 'CapsLock', 'ShiftLeft', 'ShiftRight', 'AltLeft', 'Control', 'AltRight', 'MetaLeft', 'MetaRight', 'Space'];
  
 const textField = document.createElement('textarea'),
@@ -31,9 +26,9 @@ container.append(keyBoardWrapper);
 // container.append(keyboard);
 
 class button {
-    code;
-    key;
-    element;
+    // code;
+    // key;
+    // element;
     constructor(code, key) {
         this.code = code;
         this.key = key;
@@ -109,7 +104,26 @@ class keyBoard {
 
 }
 const keyBoardEn = new keyBoard(buttonKeyEn).render(),
-      keyBoardRu = new keyBoard(buttonKeyRu).render();
+      keyBoardRu = new keyBoard(buttonKeyRu).render(),
+      keyBoardEnShift = new keyBoard(buttonKeyEnShift).render(),
+      keyBoardRuShift = new keyBoard(buttonKeyRuShift).render();
+
+// Работа с shift
+document.addEventListener('keydown', (e) => {
+    if(e.code === 'ShiftLeft' && localStorage.getItem('lang') == 'en'){
+        keyBoardWrapper.replaceChildren(keyBoardEnShift);
+    } else if(e.code === 'ShiftLeft' && localStorage.getItem('lang') == 'ru'){
+        keyBoardWrapper.replaceChildren(keyBoardRuShift);
+    }
+})
+document.addEventListener('keyup', (e) => {
+    if(e.code === 'ShiftLeft' && localStorage.getItem('lang') == 'en'){
+        keyBoardWrapper.replaceChildren(keyBoardEn);
+    } else if(e.code === 'ShiftLeft' && localStorage.getItem('lang') == 'ru'){
+        keyBoardWrapper.replaceChildren(keyBoardRu);
+    }
+})
+
 // Проверка localStorage
 if(localStorage.getItem('lang') == undefined){
     localStorage.setItem('lang', 'en')
@@ -131,151 +145,6 @@ document.addEventListener('keydown', (e) => {
     }
 })
 
-
-
-
-
-
-
-
-
-
-
-// class clickBtn {
-//     constructor(code, data){
-//         this.code = code;
-//         this.data = data;
-//     }
-
-//     render(){
-//         let keyBtn = document.createElement('div');
-//         keyBtn.classList = `key-button ${(this.code).toLowerCase()}`;
-//         keyBtn.dataset.id = this.data;
-//         keyBtn.textContent = `${this.code}`;
-//         keyboard.append(keyBtn);
-
-//         if(this.code == ' ') {
-//             keyBtn.classList = `key-button Space`;
-//         } else if(this.code == 'Shift' && shiftNum == 2) {
-//             keyBtn.classList = `key-button ${this.code}Left`;
-//             shiftNum--;
-//         } else if(this.code == 'Shift' && shiftNum == 1){
-//             keyBtn.classList = `key-button ${this.code}Right`;
-//             shiftNum++;
-//         } else if(this.code == 'Alt' && altNum == 2) {
-//             keyBtn.classList = `key-button ${this.code}l`;
-//             altNum--;
-//         } else if(this.code == 'Alt' && altNum == 1){
-//             keyBtn.classList = `key-button ${this.code}r`;
-//             altNum++;
-//         }
-
-        // document.activeElement('keydown', (e) => {
-
-        // })
-
-//         keyBtn.addEventListener('mousedown', () => {
-//             if(checkBtn.indexOf(this.code) < 0){
-//                 textField.textContent += `${this.code}`
-//             }
-//         })
-
-//         if(this.code == 'Backspace'){
-//             keyBtn.addEventListener('mousedown', () => {
-//                 textField.textContent = textField.textContent.slice(0, -1);
-//             })
-//         }
-//         if(this.code == 'Enter'){
-//             keyBtn.addEventListener('mousedown', () => {
-//                 textField.textContent += `\n`
-//             })
-//         }
-//         if(this.code == ' '){
-//             keyBtn.addEventListener('mousedown', () => {
-//                 textField.textContent += ' ';
-//             })
-//         }
-
-//     }
-// }
-
-// function createKeyBoard(arr){
-//     for(let i = 0; i < arr.length; i++){
-//         new clickBtn(arr[i], arr[i]).render();
-//     }
-// }
-
-// document.addEventListener('keydown', (e) => {
-//     if(e.altKey && e.code === 'ControlLeft' && leng === 1){
-//         keyboard.innerHTML = '';
-//         createKeyBoard(clickCodeRu);
-//         leng++;
-//     } else if(e.altKey && e.code === 'ControlLeft' && leng === 2){
-//         keyboard.innerHTML = '';
-//         createKeyBoard(clickCodeEn);
-//         leng--;
-//     }
-// })
-
-// console.log(createKeyBoard(clickCodeEn));
-
-
-
-// class keyBoardBtn {
-//     constructor(key, code){
-//         this.key = key;
-//         this.code = code;
-//     }
-//     render(){
-//         keyboard.innerHTML += `<div class="key-button" id="${this.code}">${this.key}</div>`;
-
-//         const hardBtn = document.querySelector('.key-button');
-//         document.addEventListener('keydown', (e) => {
-//             if(e.code === this.code){
-//                 hardBtn.classList.add('key_active');
-//             }
-//         })
-//         document.addEventListener('keyup', (e) => {
-//                 if(e.code === this.code){
-//                     hardBtn.classList.remove('key_active');
-//                 }
-//         })
-
-//         // keyBtn.addEventListener('mousedown', () => {
-//         //     if(checkBtn.indexOf(this.code) < 0){
-//         //         textField.textContent += `${this.code}`
-//         //     }
-//         // })
-//     }
-// }
-
-// function createKeyBoard(arr) {
-//     for(let i = 0; i < arr.length; i++) {
-//         new keyBoardBtn(arr[i][1], arr[i][0]).render()
-//     }
-// }
-
-// // Проверка localStorage
-// if(localStorage.getItem('lang') == undefined){
-//     localStorage.setItem('lang', 'en')
-// }
-// if(localStorage.getItem('lang') == 'en'){
-//     createKeyBoard(buttonKeyEn);
-// } else if(localStorage.getItem('lang') == 'ru'){
-//     createKeyBoard(buttonKeyRu);
-// }
-
-
-// // Смена языка
-// document.addEventListener('keydown', (e) => {
-//     console.log(e.code);
-//     if(e.altKey && e.code === 'ControlLeft' && localStorage.getItem('lang') == 'en'){
-//         keyboard.innerHTML = '';
-//         createKeyBoard(buttonKeyRu);
-//         localStorage.setItem('lang', 'ru');
-//     } else if(e.altKey && e.code === 'ControlLeft' && localStorage.getItem('lang') == 'ru'){
-//         keyboard.innerHTML = '';
-//         createKeyBoard(buttonKeyEn);
-//         localStorage.setItem('lang', 'en');
-//     }
-// })
+document.addEventListener('keydown', () => {
+    textField.focus();
+})
